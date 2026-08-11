4ce1adafacf69936883ae04dd4ea9da38ccfe5b0
guptapawan86 <108654677+guptapawan86@users.noreply.github.com>
2026-08-11 12:59:34 +1000

    Fix comment formatting in Byline.javtest
    test

--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/Byline.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/Byline.java
@@ -12,7 +12,7 @@
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
- */
+ *1111111111/
 package com.adobe.aem.guides.wknd.core.models;
 
 import java.util.List;
